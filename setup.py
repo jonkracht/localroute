403b2558fd1ed1716949fc525bc990f797279879
Jon Kracht <jonkracht@gmail.com>
2023-06-14 20:36:41 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Road trip app created while at Insight Data Science',
+    author='Jon Kracht',
+    license='MIT',
+)
